eca3c70535df383f7bde738a9e558276cac18aad
mahsa.mehrpoor@sats.no <mahsa.mehrpoor@sats.no>
2022-03-18 15:04:15 +0000

    updated dim customer model

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -5,25 +5,12 @@
 
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from stg.customers
-
+     select * from {{ref('stg_customers')}}
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from stg.orders
-
+     select  *  from {{ref('stg_orders')}}
 ),
 
 customer_orders as (
